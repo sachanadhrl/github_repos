aa0862430f75c65081f94a9f03c4667cdeb6ccf9
Sachan Adharullyansyah <sachanadharul@ymail.com>
2022-12-30 10:14:43 +0700

    Get user detail and repositories from api

--- src/components/RepoList.jsx
+++ src/components/RepoList.jsx
@@ -4,34 +4,32 @@ import { AiOutlineStar } from 'react-icons/ai'
 import { TbGitFork } from 'react-icons/tb'
 import { VscCircleFilled } from 'react-icons/vsc'
 
-const RepoList = () => {
+const RepoList = ({ repo }) => {
   return (    
     <div className="flex flex-col gap-3 text-sm py-10">
-      <h4>Repos</h4>
+      <h4>{ repo.name }</h4>
       <p className="text-gray-300/80">
-        This is a code repository for the corresponding YouTube video. In this video, we will create an interesting react project from scratch - A React Advice App. We're going to use React on the front en…
+        { repo.description }
       </p>
       <div className="flex flex-wrap items-center gap-2">
-        <span className="pills">react</span>
-        <span className="pills">reactjs</span>
-        <span className="pills">vuejs</span>
-        <span className="pills">tailwindcss</span>
+        {repo.topics?.map((topic, index) => (
+          <span key={index} className="pills">{ topic }</span>
+        ))}
       </div>
       <div className="flex flex-wrap items-center gap-3">
-        <span className="flex items-center text-gray-300/80">
-          <VscCircleFilled className='text-teal-500 text-lg' />
-          Javascript
-        </span>
+        {repo.language && (
+          <span className="flex items-center text-gray-300/80">
+            <VscCircleFilled className='text-teal-500 text-lg' />
+            { repo.language }
+          </span>
+        )}
         <span className="flex items-center text-gray-300/80">
           <AiOutlineStar className='mr-1' />
-          15
+          { repo.stargazers_count }
         </span>
         <span className="flex items-center text-gray-300/80">
           <TbGitFork className='mr-1' />
-          35
-        </span>
-        <span className="text-gray-300/80">
-          Updated on Aug 1
+          { repo.forks }
         </span>
       </div>
     </div>
--- src/pages/UserDetail.jsx
+++ src/pages/UserDetail.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { BiLeftArrowAlt } from 'react-icons/bi'
 import { HiOutlineUsers, HiOutlineLocationMarker, HiOutlineMail, HiOutlineLink } from 'react-icons/hi'
 import { HiOutlineBuildingOffice2 } from 'react-icons/hi2'
 
 import { RepoList } from '../components'
+import { useGetUserByUsernameQuery, useGetReposByUsernameQuery } from '../redux/services/githubApi'
 
 const UserDetail = () => {
+  const { username } = useParams()
+  const { data: user, isFetching: isFetchingUser } = useGetUserByUsernameQuery({ username })
+  const { data: userRepos, isFetching: isFetchingRepos  } = useGetReposByUsernameQuery({ username })
+
+  if (isFetchingUser || isFetchingRepos) return 'Loading data...'
+
   return (
     <>
       <div className="app__header">
@@ -14,53 +21,63 @@ const UserDetail = () => {
           <BiLeftArrowAlt className='text-3xl' />
         </Link>
         <div>
-          <h3 className='text-2xl font-bold'>John Doe</h3>
+          <h3 className='text-2xl font-bold'>{ user.name }</h3>
         </div>
       </div>
       <div className="flex justify-start items-start gap-10 px-10 py-5 xl:px-40">
         <div className='w-2/6'>
           <img
-            src="https://i.picsum.photos/id/0/5000/3333.jpg?hmac=_j6ghY5fCfSD6tvtcV74zXivkJSPIfR9B8w34XeQmvU"
-            alt="User"
+            src={user.avatar_url}
+            alt={user.name}
             className="w-full object-cover rounded-lg"
           />
-          <h2 className='mt-3'>John Doe</h2>
-          <h4 className='font-extralight text-lg'>johndoe</h4>
-          <p className='font-light mt-3'>JavaScript Enthusiast & Educator</p>
+          <h2 className='mt-3'>{ user.name }</h2>
+          <h4 className='font-extralight text-lg'>{ user.login }</h4>
+          <p className='font-light mt-3'>{ user.bio }</p>
           <div className='flex items-center gap-2 text-sm mt-3'>
             <HiOutlineUsers className='text-gray-300/80' />
             <p className='font-semibold'>
-              999.1k <span className="font-light text-gray-300/80">followers</span>
+              { user.followers } <span className="font-light text-gray-300/80">followers</span>
             </p>
             -
             <p className='font-semibold'>
-              15 <span className="font-light text-gray-300/80">following</span>
+              { user.following } <span className="font-light text-gray-300/80">following</span>
             </p>
           </div>
-          <div className='flex items-center gap-2 font-light text-sm mt-3'>
-            <HiOutlineBuildingOffice2 className='text-gray-300/80' />
-            <p>JavaScript Mastery</p>
-          </div>
-          <div className='flex items-center gap-2 font-light text-sm mt-1'>
-            <HiOutlineLocationMarker className='text-gray-300/80' />
-            <p>Croatia</p>
-          </div>
-          <div className='flex items-center gap-2 font-light text-sm mt-1'>
-            <HiOutlineMail className='text-gray-300/80' />
-            <p>contact@jsmastery.pro</p>
-          </div>
-          <div className='flex items-center gap-2 font-light text-sm mt-1'>
-            <HiOutlineLink className='text-gray-300/80' />
-            <p>jsmastery.pro</p>
-          </div>
+          {user.company && (
+            <div className='flex items-center gap-2 font-light text-sm mt-3'>
+              <HiOutlineBuildingOffice2 className='text-gray-300/80' />
+              <p>{ user.company }</p>
+            </div>
+          )}
+          {user.location && (
+            <div className='flex items-center gap-2 font-light text-sm mt-1'>
+              <HiOutlineLocationMarker className='text-gray-300/80' />
+              <p>{ user.location }</p>
+            </div>
+          )}
+          {user.email && (
+            <div className='flex items-center gap-2 font-light text-sm mt-1'>
+              <HiOutlineMail className='text-gray-300/80' />
+              <p>{ user.email }</p>
+            </div>
+          )}
+          {user.blog && (
+            <div className='flex items-center gap-2 font-light text-sm mt-1'>
+              <HiOutlineLink className='text-gray-300/80' />
+              <p>{ user.blog }</p>
+            </div>
+          )}
         </div>
         <div className='w-4/6'>
           <h2>Repositories (66)</h2>
           <div className='divide-y divide-gray-500'>
-            <RepoList />
-            <RepoList />
-            <RepoList />
-            <RepoList />
+            {userRepos?.map((repo) => (
+              <RepoList
+                key={repo.id}
+                repo={repo}
+              />
+            ))}
           </div>
         </div>
       </div>
