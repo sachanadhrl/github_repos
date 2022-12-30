d876f85227d4353b327c44ae7342fe683074dd08
Sachan Adharullyansyah <sachanadharul@ymail.com>
2022-12-30 10:13:38 +0700

    Implement search github user

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,14 @@
+import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
   const navigate = useNavigate()
+  const [searchTerm, setSearchTerm] = useState('')
 
   const handleSearch = (e) => {
     e.preventDefault()
 
-    navigate(`users/`)
+    navigate(`/search/${searchTerm}`)
   }
   
   return (
@@ -14,6 +16,8 @@ const SearchBar = () => {
       <input
         type="text"
         placeholder='Search Github user, ex: github_user'
+        value={searchTerm}
+        onChange={ (e) => setSearchTerm(e.target.value) }
       />
     </form>
   )
